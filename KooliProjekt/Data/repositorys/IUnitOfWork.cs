c8d630f9c53049cb5dc04dc0a8b661af394df53f
60710014244 <60710014244@KLASSA302-25>
2024-11-22 11:33:17 +0200

    22.11

---
+++ KooliProjekt/Data/repositorys/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KooliProjekt.Data.repositorys
+{
+    public class IUnitOfWork : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
